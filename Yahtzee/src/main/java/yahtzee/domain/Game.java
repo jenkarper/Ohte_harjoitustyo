d32c9689265e77cbddf97aac0ea73c1f0c53eb4a
jenkarper <55617257+jenkarper@users.noreply.github.com>
2020-11-24 13:14:29 +0200

    Added new class Game

--- Yahtzee/src/main/java/yahtzee/domain/Game.java
+++ Yahtzee/src/main/java/yahtzee/domain/Game.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package yahtzee.domain;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author pertjenn
  */
 public class Game {
+    private final Roll roll;
+    private final Scorecard scorecard;
+    private final Checker checker;
     
+    public Game() {
+        this.roll = new Roll();
+        this.scorecard = new Scorecard();
+        this.checker = new Checker();
+    }
+    
+    public int[] roll() {
+        roll.roll();
+        return roll.getValues();
+    }
+    
+    public boolean scoreRoll(int category, int[] dice) {
+        int points = this.checker.check(category, dice);
+        return this.scorecard.markScore(category, points);
+    }
+
+    public Roll getRoll() {
+        return roll;
+    }
+
+    public Scorecard getScorecard() {
+        return scorecard;
+    }
+
+    public Checker getChecker() {
+        return checker;
+    }
+    
+    public boolean checkHoldStatus(int die) {
+        return this.roll.getDice()[die].isHold();
+    }
+    
+    public void holdDie(int die) {
+        this.roll.getDice()[die].setHold(true);
+    }
+    
+    public void setHoldStatus(int die, boolean status) {
+        this.roll.getDice()[die].setHold(status);
+    }
 }
