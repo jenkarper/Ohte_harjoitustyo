572448f6d5296ba01d3d4276c51461bd617d4ea7
jenkarper <55617257+jenkarper@users.noreply.github.com>
2020-12-02 15:30:41 +0200

    Added PlayView and GameAlert, refactored GUI code

---
+++ Yahtzee/src/main/java/yahtzee/ui/GameAlert.java
@@ -0,0 +1,41 @@
+package yahtzee.ui;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author pertjenn
+ */
+public class GameAlert {
+    private Alert alert;
+    
+    public GameAlert() {
+        alert = new Alert(AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle(null);
+    }
+    
+    public Alert getAlert(int type, int points) {
+        if (type == 1) {
+            Label instructions = new Label("Heitä noppia klikkaamalla 'Heitä'. Voit lukita ja vapauttaa noppia klikkaamalla itse noppia. "
+                    + "Sinulla on kolme heittoa vuorossa. Kun haluat pisteyttää heiton, klikkaa 'Pisteytä'. "
+                    + "Jos heitto ei tuo pisteitä missään kategoriassa, sinun on valittava jokin kategoria ylivedettäväksi. "
+                    + "Kyseisen kategorian pistemääräksi merkitään tällöin 0.");
+            instructions.setWrapText(true);
+            alert.getDialogPane().setContent(instructions);
+            alert.getDialogPane().setPrefSize(400, 200);
+            alert.setTitle("Ohje");
+        } else if (type == 2) {
+            alert.setContentText("Olet jo käyttänyt kolme heittoa tällä kierroksella!");
+        } else if (type == 3) {
+            alert.setContentText("Yksi kategoria on valittava!");
+        } else if (type == 4) {
+            alert.setContentText("Peli ohi! Keräsit " + points + " pistettä.");
+        }
+        alert.show();
+        
+        return alert;
+    }
+}
---
+++ Yahtzee/src/main/java/yahtzee/ui/PlayView.java
@@ -0,0 +1,230 @@
+package yahtzee.ui;
+
+import java.util.Arrays;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import yahtzee.domain.Game;
+
+/**
+ *
+ * @author pertjenn
+ */
+public class PlayView {
+    private final Game game;
+    private final Button[] dieRow;
+    private final Label[] scorecard;
+    private final Label[] playerInfo;
+    private final Insets insets;
+    private final GameAlert alert;
+    
+    public PlayView(String player) {
+        this.game = new Game(player);
+        this.dieRow = createDice();
+        this.scorecard = createScorecard();
+        this.playerInfo = createPlayerInfo();
+        this.insets = new Insets(20, 20, 20, 20);
+        this.alert = new GameAlert();
+    }
+    
+    public Scene getScene() {
+        
+        // 1 CREATE LAYOUT COMPONENTS
+        // 1.1 Top: the dice
+        HBox topNode = new HBox();
+        topNode.setSpacing(30);
+        topNode.setPadding(insets);
+
+        topNode.getChildren().addAll(Arrays.asList(dieRow));
+
+        // 1.2 Left: the scorecard
+        VBox leftNode = new VBox();
+        leftNode.setSpacing(10);
+        leftNode.setPadding(insets);
+
+        leftNode.getChildren().addAll(Arrays.asList(scorecard));
+
+        // 1.3 Center: the action buttons
+        Button rollButton = new Button("Heitä");
+        styleButton(rollButton);
+        Button scoreButton = new Button("Pisteytä");
+        styleButton(scoreButton);
+
+        VBox centerNode = new VBox();
+        centerNode.setPadding(insets);
+        centerNode.setSpacing(20);
+        centerNode.getChildren().addAll(rollButton, scoreButton);
+
+        // 1.4 Bottom: the instruction and new game buttons
+        Button instructionButton = new Button("Ohje");
+        styleButton(instructionButton);
+        Button newGameButton = new Button("Aloita uusi peli");
+        styleButton(newGameButton);
+        HBox bottomNode = new HBox(instructionButton, newGameButton);
+        bottomNode.setSpacing(20);
+
+        // 1.5 Right: player info
+        VBox rightNode = new VBox();
+        rightNode.setPadding(insets);
+        rightNode.setSpacing(10);
+        
+        rightNode.getChildren().addAll(Arrays.asList(playerInfo));
+
+        // 2 CREATE PLAYVIEW LAYOUT
+        BorderPane layout = new BorderPane();
+        layout.setTop(topNode);
+        layout.setCenter(centerNode);
+        layout.setLeft(leftNode);
+        layout.setRight(rightNode);
+        layout.setBottom(bottomNode);
+
+        layout.setPadding(insets);
+        topNode.setAlignment(Pos.CENTER);
+        centerNode.setAlignment(Pos.TOP_CENTER);
+        bottomNode.setAlignment(Pos.BOTTOM_RIGHT);
+        
+        // 3 DEFINE BUTTON PLAYVIEW EVENTS
+        
+        defineButtonEvents(rollButton, scoreButton, instructionButton, newGameButton, rightNode);
+        
+        return new Scene(layout);
+    }
+    
+    private Button[] createDice() {
+        Button[] dicePane = new Button[5];
+        dicePane[0] = new Button("J");
+        dicePane[1] = new Button("A");
+        dicePane[2] = new Button("T");
+        dicePane[3] = new Button("S");
+        dicePane[4] = new Button("I");
+
+        for (Button b : dicePane) {
+            b.setStyle("-fx-background-color: #48c9b0; -fx-font-size: 2em;");
+            b.setPrefSize(50, 50);
+        }
+
+        return dicePane;
+    }
+
+    private Label[] createScorecard() {
+        Label[] sc = new Label[16];
+        sc[0] = new Label("PÖYTÄKIRJA");
+        sc[0].setFont(new Font("Arial", 18));
+        for (int i = 1; i < sc.length; i++) {
+            sc[i] = new Label(game.getScorecard().getCategories()[i]);
+            sc[i].setFont(new Font("Arial", 15));
+        }
+        return sc;
+    }
+    
+    private Label[] createPlayerInfo() {
+        Label[] info = new Label[5];
+        
+        info[0] = new Label("PELITILANNE");
+        info[0].setFont(new Font("Arial", 18));
+        
+        info[1] = new Label("Pelaaja: " + game.getPlayer());
+        info[1].setFont(new Font("Arial", 15));
+        
+        info[2] = new Label("Välisumma");
+        info[2].setFont(new Font("Arial", 15));
+        
+        info[3] = new Label("Bonus");
+        info[3].setFont(new Font("Arial", 15));
+        
+        info[4] = new Label("Yhteensä");
+        info[4].setFont(new Font("Arial", 15));
+        info[4].setStyle("-fx-font-weight: bold");
+        
+        return info;
+    }
+    
+    private void resetPlayerInfo() {
+        playerInfo[2].setText("Välisumma");
+        playerInfo[3].setText("Bonus");
+        playerInfo[4].setText("Yhteensä");
+    }
+
+    private void styleButton(Button b) {
+        b.setStyle("-fx-background-color: #e9f7ef; -fx-font-size: 1em; -fx-border-color:  #48c9b0; -fx-border-width: 1px;");
+    }
+    
+    private void reset() {
+        dieRow[0].setText("J");
+        dieRow[1].setText("A");
+        dieRow[2].setText("T");
+        dieRow[3].setText("S");
+        dieRow[4].setText("I");
+        
+        markDiceReleased();
+        resetPlayerInfo();
+        
+        for (int i = 1; i < scorecard.length; i++) {
+            scorecard[i].setText(game.getScorecard().getCategories()[i]);
+        }
+    }
+    
+    private void markDiceReleased() {
+        for (Button b : dieRow) {
+            b.setStyle("-fx-background-color: #48c9b0; -fx-font-size: 2em;");
+        }
+    }
+    
+    private void defineButtonEvents(Button rollButton, Button scoreButton, Button instructionButton, Button newGameButton, VBox rightNode) {
+        for (int i = 0; i < dieRow.length; i++) {
+            int die = i;
+            dieRow[die].setOnAction(value -> {
+                if (game.checkHoldStatus(die)) {
+                    game.setHoldStatus(die, false);
+                    dieRow[die].setStyle("-fx-background-color: #48c9b0; -fx-font-size: 2em;");
+                } else {
+                    game.setHoldStatus(die, true);
+                    dieRow[die].setStyle("-fx-background-color: #48c9b0; -fx-font-size: 2em; -fx-border-color: #138d75; -fx-border-width: 3px;");
+                }
+            });
+        }
+
+        // 4.2 Roll button
+        rollButton.setOnAction((ActionEvent event) -> {
+
+            if (game.getRollCounter() > 0) {
+                int[] newValues = game.roll();
+                for (int i = 0; i < dieRow.length; i++) {
+                    dieRow[i].setText(Integer.toString(newValues[i]));
+                }
+            } else {
+                markDiceReleased();
+                alert.getAlert(2, 0);
+            }
+        });
+
+        // 4.3 Instruction button
+        instructionButton.setOnAction((ActionEvent event) -> {
+            alert.getAlert(1, 0);
+        });
+
+        // 4.4 New game button
+        newGameButton.setOnAction((ActionEvent event) -> {
+            game.reset();
+            reset();
+            markDiceReleased();
+        });
+
+        // 4.5 Score button
+        scoreButton.setOnAction((ActionEvent event) -> {
+            markDiceReleased();
+            game.releaseAll();
+            ScoreView scoreView = new ScoreView(game, scorecard, alert, rightNode);
+            Stage scoreWindow = scoreView.getScoreView();
+            scoreWindow.show();
+        });
+    }
+}
