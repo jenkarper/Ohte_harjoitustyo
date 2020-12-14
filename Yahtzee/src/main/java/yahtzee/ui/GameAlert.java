62e94c232a4a7b534600c0a3a9898510adad9bd2
jenkarper <55617257+jenkarper@users.noreply.github.com>
2020-12-14 10:35:01 +0200

    Refactored code in GameAlert

--- Yahtzee/src/main/java/yahtzee/ui/GameAlert.java
+++ Yahtzee/src/main/java/yahtzee/ui/GameAlert.java
@@ -20,23 +20,31 @@ public class GameAlert {
     }
 
     public Alert getAlert(int type, int points) {
-        if (type == 1) {
-            Label instructions = new Label("Heitä noppia klikkaamalla 'Heitä'. Voit lukita ja vapauttaa noppia klikkaamalla itse noppia. "
-                    + "Sinulla on kolme heittoa vuorossa. Kun haluat pisteyttää heiton, klikkaa 'Pisteytä'. "
-                    + "Jos heitto ei tuo pisteitä missään kategoriassa, sinun on valittava jokin kategoria ylivedettäväksi. "
-                    + "Kyseisen kategorian pistemääräksi merkitään tällöin 0.");
-            instructions.setWrapText(true);
-            alert.getDialogPane().setContent(instructions);
-            alert.getDialogPane().setPrefSize(400, 200);
-            alert.setTitle("Ohje");
-        } else if (type == 2) {
-            alert.setContentText("Olet jo käyttänyt kolme heittoa tällä kierroksella!");
-        } else if (type == 3) {
-            alert.setContentText("Yksi kategoria on valittava!");
-        } else if (type == 4) {
-            alert.setContentText("Peli ohi! Keräsit " + points + " pistettä.");
-        } else if (type == 5) {
-            alert.setContentText("Heitä vähintään kerran!");
+        switch (type) {
+            case 1:
+                Label instructions = new Label("Heitä noppia klikkaamalla 'Heitä'. Voit lukita ja vapauttaa noppia klikkaamalla itse noppia. "
+                        + "Sinulla on kolme heittoa vuorossa. Kun haluat pisteyttää heiton, klikkaa 'Pisteytä'. "
+                        + "Jos heitto ei tuo pisteitä missään kategoriassa, sinun on valittava jokin kategoria ylivedettäväksi. "
+                        + "Kyseisen kategorian pistemääräksi merkitään tällöin 0.");
+                instructions.setWrapText(true);
+                alert.getDialogPane().setContent(instructions);
+                alert.getDialogPane().setPrefSize(400, 200);
+                alert.setTitle("Ohje");
+                break;
+            case 2:
+                alert.setContentText("Olet jo käyttänyt kolme heittoa tällä kierroksella!");
+                break;
+            case 3:
+                alert.setContentText("Yksi kategoria on valittava!");
+                break;
+            case 4:
+                alert.setContentText("Peli ohi! Keräsit " + points + " pistettä.");
+                break;
+            case 5:
+                alert.setContentText("Heitä vähintään kerran!");
+                break;
+            default:
+                break;
         }
         alert.show();
 
